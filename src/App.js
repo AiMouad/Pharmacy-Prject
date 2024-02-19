466880da1866c82bedc73212b9f524705b669685
Mouad Aichi <mouad14anes@gmail.com>
2024-02-19 18:37:04 +0100

    Hi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
-//import Sidebar from './components/Sidebar/Sidebar';
+import Sidebar from './components/Sidebar/Sidebar';
 import Notification from './components/Notification/Notification'
+//import NotificationMessegeUp from './components/Notification/NotificationMessegeUp';
 function App() {
   return (
    <div>
--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,38 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import './Notification.css';
+import './Notification.css'
 
 const Notification = () => {
-    const [lowStockMedicines, setLowStockMedicines] = useState([]);
-    const [expiringSoonDrugs, setExpiringSoonDrugs] = useState([]);
+  const [lowStockMedicines, setLowStockMedicines] = useState([]);
+  const [expiringSoonDrugs, setExpiringSoonDrugs] = useState([]);
 
-    const [showLowStockDescription, setShowLowStockDescription] = useState(false);
-    const [showExpiringSoonDescription, setShowExpiringSoonDescription] = useState(false);
+  const [showLowStockDescription, setShowLowStockDescription] = useState(false);
+  const [showExpiringSoonDescription, setShowExpiringSoonDescription] = useState(false);
 
-    const Edit = () => {
-        window.location.href = "https://10015.io/tools/css-gradient-generator";
+  const Edit = () => {
+    window.location.href = "";
+  };
+
+  const isExpiringSoon = (validity) => {
+    const targetDate = new Date();
+    targetDate.setHours(0, 0, 0, 0); // Set the hours, minutes, seconds, and milliseconds to 0
+    const medicineDate = new Date(validity);
+    const diffInTime = medicineDate.getTime() - targetDate.getTime();
+    const diffInDays = diffInTime / (1000 * 3600 * 24);
+    return diffInDays <= 10;
+  }
+
+  useEffect(() => {
+    // Example data
+    const medicineStock = {
+      MedicineA: 5,
+      MedicineB: 30,
+      MedicineC: 1,
     };
 
-    useEffect(() => {
-        // Example data
-        const medicineStock = {
-            MedicineA: 5,
-            MedicineB: 30,
-            MedicineC: 1,
-        };
+    const drugs = [
+      { name: 'Drug X', validity: '2025-07-31' },
+      { name: 'Drug Y', validity: '2024-03-01' },
+      { name: 'Drug Z', validity: '202-02-30' },
+    ];
 
-        const drugs = [
-            { name: 'Drug X', validity: '2023-07-31' },
-            { name: 'Drug Y', validity: '2023-08-15' },
-            { name: 'Drug Z', validity: '2024-02-28' },
-        ];
+    const lowStockMedicinesFiltered = Object.entries(medicineStock).filter(([name, qty]) => qty < 10);
+    const expiringSoonDrugsFiltered = drugs.filter(({ validity }) => isExpiringSoon(validity));
 
-        const lowStockMedicinesFiltered = Object.entries(medicineStock).filter(([name, qty]) => qty < 10);
-        const expiringSoonDrugsFiltered = drugs.filter(({ validity }) => new Date(validity) < new Date('2023-09-01'));
-
-        setLowStockMedicines(lowStockMedicinesFiltered);
-        setExpiringSoonDrugs(expiringSoonDrugsFiltered);
-    }, []);
-
+    setLowStockMedicines(lowStockMedicinesFiltered);
+    setExpiringSoonDrugs(expiringSoonDrugsFiltered);
+  }, []);
     const toggleLowStockDescription = () => {
         setShowLowStockDescription(prevState => !prevState);
     };
@@ -46,11 +54,14 @@ const Notification = () => {
             <div className="sidebar">This is the sidebar</div>
             <div className="Noti">
                 <div className='up'>
+                <button className="But">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 25" fill="none"><path fill-rule="evenodd" fill="#7D8590" d="m11.9572 4.31201c-3.35401 0-6.00906 2.59741-6.00906 5.67742v3.29037c0 .1986-.05916.3927-.16992.5576l-1.62529 2.4193-.01077.0157c-.18701.2673-.16653.5113-.07001.6868.10031.1825.31959.3528.67282.3528h14.52603c.2546 0 .5013-.1515.6391-.3968.1315-.2343.1117-.4475-.0118-.6093-.0065-.0085-.0129-.0171-.0191-.0258l-1.7269-2.4194c-.121-.1695-.186-.3726-.186-.5809v-3.29037c0-1.54561-.6851-3.023-1.7072-4.00431-1.1617-1.01594-2.6545-1.67311-4.3019-1.67311zm-8.00906 5.67742c0-4.27483 3.64294-7.67742 8.00906-7.67742 2.2055 0 4.1606.88547 5.6378 2.18455.01.00877.0198.01774.0294.02691 1.408 1.34136 2.3419 3.34131 2.3419 5.46596v2.97007l1.5325 2.1471c.6775.8999.6054 1.9859.1552 2.7877-.4464.795-1.3171 1.4177-2.383 1.4177h-14.52603c-2.16218 0-3.55087-2.302-2.24739-4.1777l1.45056-2.1593zm4.05187 11.32257c0-.5523.44772-1 1-1h5.99999c.5523 0 1 .4477 1 1s-.4477 1-1 1h-5.99999c-.55228 0-1-.4477-1-1z" clip-rule="evenodd"></path></svg>
+                    <span className="notifications-count">5</span>
+                </button>
                 Notification
                 </div>
                 <div className='down'>
                 <div className='info'>
-                    <p></p>
                 <button class="fancy" onClick={toggleLowStockDescription}>
                     {showLowStockDescription ? 'Hide Low Stock Description' : 'Show Low Stock Description'}
                 </button>
